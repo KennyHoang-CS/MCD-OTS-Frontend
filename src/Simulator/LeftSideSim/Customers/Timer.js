37624a437a8ed18d82a49e605746ec2c786f8ada
KennyHoang-CS <kennyhoangcs@gmail.com>
2021-06-29 19:37:13 -0700

    minor changes to mobile

--- src/Simulator/LeftSideSim/Customers/Timer.js
+++ src/Simulator/LeftSideSim/Customers/Timer.js
@@ -32,7 +32,7 @@ export default function Timer() {
 
     return (
         <div>
-            <div>
+            <div className="Timer">
                 <span>{("0" + Math.floor(((time / 60000) % 60))).slice(-2)}:</span>
                 <span>{("0" + Math.floor(((time / 1000) % 60))).slice(-2)}:</span>
                 <span>{("0" + Math.floor(((time / 10) % 100))).slice(-2)}</span>
