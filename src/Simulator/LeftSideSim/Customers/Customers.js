bfb34a96f09bad02514321f95a986deb8738f59c
KennyHoang-CS <kennyhoangcs@gmail.com>
2021-07-04 01:19:21 -0700

    adding back user input validation

--- src/Simulator/LeftSideSim/Customers/Customers.js
+++ src/Simulator/LeftSideSim/Customers/Customers.js
@@ -52,14 +52,12 @@ export default function Customers() {
         
         // if user input passes, load in next customer. 
         if (orderPassed) {
-            //dispatch(setMessage(`Customer #${++customerIdx} passed.`))
-           // dispatch(nextCustomer());   // get the next customer. 
-           // dispatch(resetCurrentOrder()); // clear the user input for next customer. 
+            dispatch(setMessage(`Customer #${++customerIdx} passed.`))
+            dispatch(nextCustomer());   // get the next customer. 
+            dispatch(resetCurrentOrder()); // clear the user input for next customer. 
         } else if (!orderPassed) {  
-           // dispatch(setMessage(`Customer #${++customerIdx} failed.`))
+            dispatch(setMessage(`Customer #${++customerIdx} failed.`))
         } 
-        dispatch(nextCustomer());   // get the next customer. 
-            dispatch(resetCurrentOrder()); // clear the user input for n
     }
 
     // After the last customer, the game is over. 
